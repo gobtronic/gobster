c823b3e79cc35d06e92613597ef3b93a0c2d2917
Antoine POPINEAU <antoine@popineau.eu>
2024-09-24 17:45:21 +0200

    Fixed alignement issue and date post dates.

--- cmd/gobster/display/list/list.go
+++ cmd/gobster/display/list/list.go
@@ -2,6 +2,7 @@ package list
 
 import (
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/bubbles/key"
 	bubblelist "github.com/charmbracelet/bubbles/list"
@@ -20,6 +21,7 @@ var tagDefaultBackground lipgloss.AdaptiveColor = lipgloss.AdaptiveColor{Light: 
 var tagRedBackground lipgloss.AdaptiveColor = lipgloss.AdaptiveColor{Light: "#3b1719", Dark: "#cf8488"}
 var tagGreyBackground lipgloss.AdaptiveColor = lipgloss.AdaptiveColor{Light: "#c2c2c2", Dark: "#d4d4d4"}
 var tagBlueBackground lipgloss.AdaptiveColor = lipgloss.AdaptiveColor{Light: "#15293d", Dark: "#9fbfde"}
+var dimForeground lipgloss.AdaptiveColor = lipgloss.AdaptiveColor{Light: "#343434", Dark: "#727272"}
 
 var catBackgrounds map[string]lipgloss.AdaptiveColor = map[string]lipgloss.AdaptiveColor{
 	"ask":        tagRedBackground,
@@ -39,13 +41,14 @@ type Item struct {
 	title      string
 	categories []string
 	Url        string
+	date       *time.Time
 }
 
 func (i Item) FilterValue() string { return i.title + strings.Join(i.categories, " ") }
 
 type itemDelegate struct{}
 
-func (d itemDelegate) Height() int                                   { return 1 }
+func (d itemDelegate) Height() int                                   { return 2 }
 func (d itemDelegate) Spacing() int                                  { return 0 }
 func (d itemDelegate) Update(_ tea.Msg, _ *bubblelist.Model) tea.Cmd { return nil }
 
@@ -72,6 +75,7 @@ func NewList(feed *gofeed.Feed, initialTermSize [2]int) bubblelist.Model {
 			title:      v.Title,
 			categories: v.Categories,
 			Url:        v.Link,
+			date:       v.PublishedParsed,
 		})
 	}
 	l.SetItems(items)
